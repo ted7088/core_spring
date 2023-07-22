af8b703063c6abe31fe90c528b8b5f88ef639a3e
ted7088 <ted3131@naver.com>
2023-07-22 12:25:57 +0900

    commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,8 +9,7 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-public class AppConfig {  //공연 기획자의 역할 외부에서 주입
-
+public class AppConfig {  //공연 기획자의 역할 외부에서 주입   스프링
     public MemberService memberService() {
         return new MemberServiceImpl(membeerRepository());
     }
