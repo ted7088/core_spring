ee72e7fc9b0df36d81d94c7abf7741cadef5b498
ted7088 <ted3131@naver.com>
2023-07-22 12:38:23 +0900

    commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {  //공연 기획자의 역할 외부에서 주입   스프링
+
+@Configuration
+public class AppConfig {  //공연 기획자의 역할 외부에서 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(membeerRepository());
     }
 
-    private static MemoryMemberRepository membeerRepository() {
+    @Bean
+    public static MemoryMemberRepository membeerRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(membeerRepository(), discountPolicy());
     }
 
-    public DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
