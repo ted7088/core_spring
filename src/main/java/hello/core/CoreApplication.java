c5efd2501e4d18a7364f1265f6bb3800049ea4f0
ted7088 <ted3131@naver.com>
2023-07-27 14:05:50 +0900

    commit

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-
+ //메인
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
